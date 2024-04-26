4a706a280dc038e88a09975ed0d8ca82bd1f3064
IliasIhadian <i.ihadian@gmail.com>
2024-04-26 06:03:02 +0200

    build the components Pizza Header Menu and Footer

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -1,9 +1,111 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
 
 function App() {
-  return <h1>Henlo</h1>;
+  return (
+    <>
+      <Header />
+      <Menu />
+      <Footer />
+    </>
+  );
 }
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Ilias Pizzeria</h1>;
+    </header>
+  );
+};
+
+const Menu = () => {
+  return (
+    <>
+      <main className="menu">
+        <h2>Our Menu</h2>
+        <Pizza
+          name="Pizza ya hobi"
+          ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+          photoName="pizzas/spinaci.jpg"
+          price={10}
+        />
+        <Pizza />
+        <Pizza />
+      </main>
+    </>
+  );
+};
+
+const Pizza = (props) => {
+  return (
+    <>
+      <div className="pizza">
+        <img src={props.photoName} alt={props.name}></img>
+        <div>
+          <h3>{props.name}</h3>
+          <p>{props.ingredients}</p>
+          <span>{props.price}</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}.We're currently open!
+    </footer>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
